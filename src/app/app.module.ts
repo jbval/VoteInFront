c83e2d950902068f1f28272e150eba77cf4ffa84
Orkad <nicolas.gidon.43@gmail.com>
2017-08-28 18:09:29 +0200

    Ajout du système de routage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { routes } from './app.router';
+import { AboutComponent } from './about/about.component';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routes
   ],
   providers: [],
   bootstrap: [AppComponent]
