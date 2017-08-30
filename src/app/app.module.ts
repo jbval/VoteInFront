d48f5b9e5cf885f15b519413c1c4ff2e918ad62f
Orkad <nicolas.gidon.43@gmail.com>
2017-08-30 15:41:45 +0200

    Remplacement ng2-materialize par angular2-materialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterializeModule } from "angular2-materialize";
+import { routes } from './app.router';
+import { FormsModule } from '@angular/forms'; 
 
-import { MaterializeModule } from 'ng2-materialize';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-
-import { routes } from './app.router';
 import { AboutComponent } from './about/about.component';
 import { PollComponent} from './poll/poll.component';
 import { CreatePollComponent } from './poll/create/createPoll.component';
@@ -25,6 +25,7 @@ import { VotePollComponent } from './poll/vote/votePoll.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     MaterializeModule.forRoot(),
     routes
   ],
--- src/app/poll/create/createPoll.component.ts
+++ src/app/poll/create/createPoll.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Proposition } from '../../model/proposition';
 
 @Component({
   selector: 'app-create',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./createPoll.component.css']
 })
 export class CreatePollComponent implements OnInit {
-
+  private propositions: Array<Proposition>;
+  private test: string;
   constructor() { }
 
   ngOnInit() {
+    this.propositions = new Array<Proposition>();
+    var p = new Proposition();
+    p.Id = 1;
+    p.Name = "première proposition";
+    p.Description = "description de la première proposition";
+    this.propositions.push(p);
+    var p = new Proposition();
+    p.Id = 2;
+    p.Name = "deuxième proposition";
+    p.Description = "description de la deuxième proposition";
+    this.propositions.push(p);
+  }
+
+  addEmptyProposition(){
+    let props = this.propositions
+    setTimeout(function() {
+
+      console.log("addEmptyProposition");
+      var p = new Proposition();
+      p.Id = 3;
+      p.Name = "nouvelle proposition";
+      p.Description = "";
+      props.push(p);
+    }, 10);
   }
 
 }
