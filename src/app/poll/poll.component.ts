c9c9f10ae1a87e07c52c77fbcce7d614936d589c
Orkad <nicolas.gidon.43@gmail.com>
2017-09-01 16:38:48 +0200

    Mise a jour du design général sur les page scrutin et création scrutin

--- src/app/poll/poll.component.ts
+++ src/app/poll/poll.component.ts
@@ -1,29 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { Scrutin } from '../model/model';
+import { Router }from '@angular/router';
 
 import { ScrutinApiService } from '../services/api/scrutinApi.service';
 import { ModeScrutinApiService } from '../services/api/modeScrutinApi.service';
 import { SharedService } from '../services/shared.service';
 
+
 @Component({
   selector: 'app-poll',
   templateUrl: './poll.component.html',
-  styleUrls: ['./poll.component.css']
+  styleUrls: ['./poll.component.sass']
 })
 export class PollComponent implements OnInit {
   private pollList:Scrutin[];
 
-  constructor(private scrutinApiService:ScrutinApiService) {}
+  constructor(private scrutinApiService:ScrutinApiService,
+    private router:Router) {}
 
   ngOnInit() {
     this.scrutinApiService.get().subscribe(
       res => {
         this.pollList = res;
       });
-  }
+  };
 
-  paginate(pageNumber:number){
-    
-  }
+  navigateTo(route:string){
+    this.router.navigate([route]);
+  };
 
+  goToVote(scrutinId:number){
+    this.router.navigate(["poll/"+scrutinId]);
+  };
 }
