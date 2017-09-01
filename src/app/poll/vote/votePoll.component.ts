61adc8ec6b68d424c11d6739dff23e0f3912f9e7
unknown <Admin@PC-DEV10-3.myapollossc.com>
2017-09-01 09:33:44 +0200

    Pre-pull

--- src/app/poll/vote/votePoll.component.ts
+++ src/app/poll/vote/votePoll.component.ts
@@ -7,6 +7,8 @@ import { Scrutin } from '../../model/scrutin';
 import { ScrutinApiService } from '../../services/api/scrutinApi.service';
 import { SharedService } from '../../services/shared.service';
 
+import { $ } from "jquery";
+
 @Component({
   selector: 'app-vote',
   templateUrl: './votePoll.component.html',
@@ -14,15 +16,12 @@ import { SharedService } from '../../services/shared.service';
 })
 export class VotePollComponent implements OnInit {
   private pollId: string;
-  private scrutin: Scrutin;
+  private scrutin: Scrutin = new Scrutin;
+  private buttonValue: String = "Voter";
   
   constructor(private activatedRoute: ActivatedRoute, private scrutinApiService:ScrutinApiService, private sharedService:SharedService) { }
 
   ngOnInit() {
-    
-  }
-
-  ngAfterViewInit(){
     this.activatedRoute.params.subscribe((params: Params) => {
       this.pollId = params['id'];
       this.scrutinApiService.getById(this.pollId).subscribe(
@@ -30,6 +29,20 @@ export class VotePollComponent implements OnInit {
           this.scrutin = res;
         });
     })
+    
+  }
+
+  ngAfterViewInit(){
+    
+  }
+
+  openDropdown(index: string) {
+    $('.dropdown-button').dropdown('open');
+  }
+
+  selectGrade(proposition: Proposition, grade: string) {
+    this.buttonValue = grade;
+    console.log(proposition);
   }
 
 }
