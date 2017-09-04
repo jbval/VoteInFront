667ba7958251c5844e9931f157d3dbf6b5efcb49
REVI <remy.villain@apollossc.com>
2017-09-04 12:02:39 +0200

    index.js node

--- src/server/index.js
+++ src/server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 // const bodyParser = require('body-parser');
 
-const root = './public';
+const root = './dist';
 const public = process.env.PUBLIC || `${root}`;
 const app = express();
 
@@ -11,8 +11,8 @@ const app = express();
 app.use(express.static(public));
 console.log(`serving ${public}`);
 app.get('*', (req, res) => {
-  res.sendFile(`index.html`, { root: root });
+  res.sendFile(req.url, { root: root });
 });
 
-const port = process.env.PORT || '3000';
+const port = process.env.PORT || '80';
 app.listen(port, () => console.log(`API running on localhost:${port}`));
